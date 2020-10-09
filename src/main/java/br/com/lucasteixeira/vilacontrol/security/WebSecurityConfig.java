3606008256734aa46080e222111cf9369692409f
Lucas <lucas.teixeira34@gmail.com>
2020-10-09 10:32:43 -0300

    Correção de bug /error

--- src/main/java/br/com/lucasteixeira/vilacontrol/security/WebSecurityConfig.java
+++ src/main/java/br/com/lucasteixeira/vilacontrol/security/WebSecurityConfig.java
@@ -46,7 +46,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception{
-        web.ignoring().antMatchers("/css/**", "/fonts/**", "/resources/**", "/img/**", "/js/**", "/vendor/**");
+        web.ignoring().antMatchers("/static/**", "/resources/**", "/assets/**", "/contactform/**", "/css/**", "/fonts/**", "/img/**", "/js/**", "/vendor/**", "/error");
     }
 
 }
